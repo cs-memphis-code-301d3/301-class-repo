98a9826738f208fc149fbeb662e8c3af423ac72e
kevin-codecrew <kevin@code-crew.org>
2021-09-29 06:22:23 -0500

    lecture

--- week 3/class-06/demo/src/App.js
+++ week 3/class-06/demo/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import MyComponent from './MyComponent';
 
 class App extends React.Component {
   constructor(props){
@@ -11,18 +12,19 @@ class App extends React.Component {
   }
 
   getLocation = async () => {
-    const locQuery = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${this.state.searchQuery}&format=json`;
-//    const locQuery = `https://us1.locationiq.com/v1/search.php?key=pk.c50f5eca19e1c38a645400ec7dcb01fe&q=${this.state.searchQuery}&format=json`;
+//    const locQuery = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${this.state.searchQuery}&format=json`;
+    const locQuery = `https://us1.locationiq.com/v1/search.php?key=pk.c50f5eca19e1c38a645400ec7dcb01fe&q=${this.state.searchQuery}&format=json`;
     console.log(locQuery);
     const API = locQuery;
     const res = await axios.get(API);
-    console.log(res.data[0])
+    console.log(res.data[0]);
     this.setState({ location:res.data[0] });
   }
 
   render() {
     return(
       <div>
+        <MyComponent/>
         <input onChange={(e) => this.setState({ searchQuery: e.target.value })} placeholder="search for a city"></input>
         <button onClick={this.getLocation}>Explore!</button>
         {this.state.location.place_id && 
---
+++ week 3/class-06/demo/src/MyComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class MyComponent extends Component {
+    render() {
+        return (
+            <div>
+                <h1>My Funky Component!</h1>
+                <h2>For real</h2>
+            </div>
+        )
+    }
+}
