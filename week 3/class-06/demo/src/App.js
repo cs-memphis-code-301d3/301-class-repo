b8607a6a2ccbf8bdbd8497c56c93b02b8520c36b
kevin-codecrew <kevin@code-crew.org>
2021-09-30 08:01:33 -0500

    In-class repo updates

--- week 3/class-06/demo/src/App.js
+++ week 3/class-06/demo/src/App.js
@@ -12,24 +12,29 @@ class App extends React.Component {
   }
 
   getLocation = async () => {
-//    const locQuery = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${this.state.searchQuery}&format=json`;
-    const locQuery = `https://us1.locationiq.com/v1/search.php?key=pk.c50f5eca19e1c38a645400ec7dcb01fe&q=${this.state.searchQuery}&format=json`;
-    console.log(locQuery);
-    const API = locQuery;
-    const res = await axios.get(API);
-    console.log(res.data[0]);
-    this.setState({ location:res.data[0] });
+  //  const locQuery = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${this.state.searchQuery}&format=json`;
+    // const locQuery = `https://us1.locationiq.com/v1/search.php?key=pk.c50f5eca19e1c38a645400ec7dcb01fe&q=${this.state.searchQuery}&format=json`;
+    // console.log(locQuery);
+    // const API = locQuery;
+    const res = await axios.get("http://localhost:3001/");
+    // console.log(res.data[0]);
+    console.log(res);
+    // this.setState({ location:res.data[0] });
+    this.setState({ location:res });
   }
 
   render() {
+    this.getLocation();
+
     return(
       <div>
-        <MyComponent/>
+        <div>{this.state.location}</div>
+        {/* <MyComponent/>
         <input onChange={(e) => this.setState({ searchQuery: e.target.value })} placeholder="search for a city"></input>
-        <button onClick={this.getLocation}>Explore!</button>
-        {this.state.location.place_id && 
+        <button onClick={this.getLocation}>Explore!</button> */}
+        {/* {this.state.location.place_id && 
           <h2>The city is: {this.state.location.display_name}</h2>
-        }
+        } */}
       </div>
     )
   }
