d53a5a554ebdafbcaca9e61a1b62346d132fb7e0
kevin-codecrew <kevin@code-crew.org>
2021-10-11 14:55:39 -0500

    Completed lecture code. Added some other sample components.

--- week 5/class-11/lab/client/src/App.js
+++ week 5/class-11/lab/client/src/App.js
@@ -1,22 +1,27 @@
+import React from 'react';
 import './App.css';
-import { useAuth0 } from "@auth0/auth0-react";
+import { withAuth0 } from '@auth0/auth0-react';
 import LoginButton from './components/LoginButton';
 import LogoutButton from './components/LogoutButton';
-
-function App() {
-  return (
-    <div>
-      <LoginButton />
-      <LogoutButton />
-      {/* If the user as logged in, display some user info stuff */}
-      {this.props.auth0.isAuthenticated &&
-        <>
-          <Profile />
-          <Content />
-        </>
-      }
-    </div>
-  );
+import Profile from './components/Profile';
+import Content from './components/Content';
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <LoginButton />
+        <LogoutButton />
+        {/* If the user as logged in, display some user info stuff */}
+        {this.props.auth0.isAuthenticated &&
+          <>
+            <Profile />
+            <Content />
+          </>
+        }
+      </>
+    )
+  }
 }
 
-export default App;
+export default withAuth0(App);
+
